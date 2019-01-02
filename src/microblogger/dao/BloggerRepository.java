66a5f359d96e0776aae53deabe1aaacbffe6aa40
L <lnpeng@gmail.com>
2019-01-02 16:29:37 -0500

    switch to maven build

--- src/microblogger/dao/BloggerRepository.java
+++
@@ -1,9 +0,0 @@
-package microblogger.dao;
-
-import microblogger.dao.entity.Blogger;
-
-public interface BloggerRepository {
-    Blogger save(Blogger bloger);
-
-    Blogger findByUsername(String username);
-}
